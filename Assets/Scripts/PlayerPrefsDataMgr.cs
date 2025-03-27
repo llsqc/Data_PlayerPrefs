4885a40615250276a09e2891ea3ebb3c3eb73ff2
isolation <23796978@qq.com>
2025-03-27 17:05:12 +0800

    feat: 创建数据管理类

--- Assets/Scripts/PlayerPrefsDataMgr.cs
+++ Assets/Scripts/PlayerPrefsDataMgr.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,17 +7,24 @@ using UnityEngine;
 public class PlayerPrefsDataMgr
 {
     private static PlayerPrefsDataMgr _instance = new PlayerPrefsDataMgr();
-    
+
     public static PlayerPrefsDataMgr Instance
     {
-        get
-        {
-            return _instance;
-        }
+        get { return _instance; }
     }
-    
+
     private PlayerPrefsDataMgr()
     {
+    }
+    
+    public void SaveData(object data, string keyName)
+    {
         
+    }
+    
+    public object LoadData(Type type, string keyName)
+    {
+        
+        return null;
     }
 }
