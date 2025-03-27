f9c8da312fba95d893139f5057fc7098bf0deda5
isolation <23796978@qq.com>
2025-03-27 21:53:37 +0800

    feat: 存储常用成员

--- Assets/Scripts/PlayerPrefsDataMgr.cs
+++ Assets/Scripts/PlayerPrefsDataMgr.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Reflection;
 using UnityEngine;
 
 
@@ -16,15 +17,44 @@ public class PlayerPrefsDataMgr
     private PlayerPrefsDataMgr()
     {
     }
-    
+
     public void SaveData(object data, string keyName)
     {
-        
+        Type dataType = data.GetType();
+        FieldInfo[] infos = dataType.GetFields();
+        FieldInfo info;
+
+        for (int i = 0; i < infos.Length; i++)
+        {
+            info = infos[i];
+            string saveKeyName = keyName + "_" + dataType.Name + "_" + info.FieldType.Name + "_" + info.Name;
+            SaveValue(info.GetValue(data), saveKeyName);
+        }
     }
-    
+
+    public void SaveValue(object value, string keyName)
+    {
+        Type fieldType = value.GetType();
+        if (fieldType == typeof(int))
+        {
+            PlayerPrefs.SetInt(keyName, (int)value);
+        }
+        else if (fieldType == typeof(float))
+        {
+            PlayerPrefs.SetFloat(keyName, (float)value);
+        }
+        else if (fieldType == typeof(string))
+        {
+            PlayerPrefs.SetString(keyName, value.ToString());
+        }
+        else if (fieldType == typeof(bool))
+        {
+            PlayerPrefs.SetInt(keyName, (bool)value ? 1 : 0);
+        }
+    }
+
     public object LoadData(Type type, string keyName)
     {
-        
         return null;
     }
 }
